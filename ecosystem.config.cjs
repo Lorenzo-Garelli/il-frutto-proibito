673e38b5d1cae35b2a45942e797e9711b32a92c0
Lorenzo Garelli <142790274+Lorenzo-Garelli@users.noreply.github.com>
2026-04-15 00:10:09 +0200

    Add ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+      name: "il-frutto-proibito",
+      script: "./dist/index.mjs",
+      env: { NODE_ENV: "production", PORT: 3000 },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M"
+    }]
+  };
